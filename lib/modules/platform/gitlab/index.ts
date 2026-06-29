b523f084cda0fd587302c320bc522071787b9853
Sam McBroom <sam.mcbroom@appian.com>
2026-06-29 14:48:37 -0700

    fix(gitlab): update additional references for custom forking behavior

--- lib/modules/platform/gitlab/index.ts
+++ lib/modules/platform/gitlab/index.ts
@@ -520,7 +520,7 @@ export async function getBranchStatus(
 export async function getPrList(): Promise<Pr[]> {
   return await GitlabPrCache.getPrs(
     gitlabApi,
-    config.repository,
+    config.mergeRequestRepository,
     botUserName,
     !!config.ignorePrAuthor,
   );
@@ -759,7 +759,7 @@ export async function createPr({
   const pr = prInfo(res.body);
   await GitlabPrCache.setPr(
     gitlabApi,
-    config.repository,
+    config.mergeRequestRepository,
     botUserName,
     pr,
     !!config.ignorePrAuthor,
@@ -928,7 +928,7 @@ export async function findPr({
   if (includeOtherAuthors) {
     // PR might have been created by anyone, so don't use the cached Renovate MR list
     const response = await gitlabApi.getJsonUnchecked<GitLabMergeRequest[]>(
-      `projects/${config.repository}/merge_requests?source_branch=${branchName}&state=opened`,
+      `projects/${config.mergeRequestRepository}/merge_requests?source_branch=${branchName}&state=opened`,
     );
 
     const { body: mrList } = response;
@@ -1242,7 +1242,7 @@ export async function addAssignees(
       }
     }
     const url = `projects/${
-      config.repository
+      config.mergeRequestRepository
     }/merge_requests/${iid}?${getQueryString({
       'assignee_ids[]': assigneeIds,
     })}`;
@@ -1269,7 +1269,7 @@ export async function addReviewers(
 
   let mr: GitLabMergeRequest;
   try {
-    mr = await getMR(config.repository, iid);
+    mr = await getMR(config.mergeRequestRepository, iid);
   } catch (err) {
     logger.warn({ err }, 'Failed to get existing reviewers');
     return;
@@ -1427,13 +1427,13 @@ export async function ensureComment({
   if (!commentId) {
     await addComment(number, body);
     logger.debug(
-      { repository: config.repository, issueNo: number },
+      { repository: config.mergeRequestRepository, issueNo: number },
       'Added comment',
     );
   } else if (commentNeedsUpdating) {
     await editComment(number, commentId, body);
     logger.debug(
-      { repository: config.repository, issueNo: number },
+      { repository: config.mergeRequestRepository, issueNo: number },
       'Updated comment',
     );
   } else {
